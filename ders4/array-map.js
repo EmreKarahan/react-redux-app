a559168a126c5b03c2bc019d1e8104a970e54231
Emre Karahan <emre.karahan@dogangazetecilik.com.tr>
2017-09-08 16:55:39 +0300

    ok

---
+++ ders4/array-map.js
@@ -0,0 +1,24 @@
+// const arr = [1,2,3,4,5,6,7,8,9,10];
+
+// const newArr = arr.map(item=> item* 2);
+
+// console.log(newArr);
+
+const users = [
+  {
+    id: 1,
+    username: 'emre',
+  },
+  {
+    id: 2,
+    username: 'eren',
+  },
+  {
+    id: 3,
+    username: 'bahar',
+  },
+  {
+    id: 4,
+    username: 'badit',
+  },
+];
