b95dc60f2dd97e64d483b91c7e6728d040bb473c
emre <emre@localhost.localdomain>
2017-09-09 04:41:55 +0300

    Header ve Logo

---
+++ src/components/Logo.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import logo from '../img/logo.svg';
+
+const Logo = () => <img width="155" height="46" src={logo} className="Logo" alt="tepsi.co" />;
+export default Logo;
